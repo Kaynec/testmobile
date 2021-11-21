0857a84ec48c84d4a224d9ed8b3ee77e5db7716a
Kaynec <once462@gmail.com>
2021-11-21 22:05:37 +0330

    Added my-profile profile-points profile-inbox profile-edit

--- src/modules/student-modules/message/profile-message.vue
+++ src/modules/student-modules/message/profile-message.vue
@@ -39,9 +39,10 @@ export default defineComponent({
 <style lang="scss" scoped>
 .message {
   position: fixed;
-  width: 100%;
-  height: 100%;
-  inset: 0;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
   background: rgba(0, 0, 0, 0.5);
   background-repeat: no-repeat;
   background-attachment: fixed;
